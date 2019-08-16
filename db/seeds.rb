f66bc17de2fc3ddcb0dd7bf48a1f0c3176ab01b7
jpAppsRepo <misheel@Misheels-MacBook-Pro.local>
2019-08-16 09:52:10 +0900

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # add doctors data
-User.create(email: "user1@gmail.com", password: "user1-pass", role_id: 0)
-User.create(email: "user2@gmail.com", password: "user2-pass", role_id: 0)
-User.create(email: "user3@gmail.com", password: "user3-pass", role_id: 0)
-User.create(email: "user4@gmail.com", password: "user4-pass", role_id: 0)
-User.create(email: "user5@gmail.com", password: "user5-pass", role_id: 0)
-User.create(email: "user6@gmail.com", password: "user6-pass", role_id: 0)
-User.create(email: "user7@gmail.com", password: "user7-pass", role_id: 0)
-User.create(email: "user8@gmail.com", password: "user8-pass", role_id: 0)
-User.create(email: "admin1@gmail.com", password: "admin-password", role_id: 1)
+User.create(email: "user1@gmail.com", password: "user1-pass191", role_id: 0)
+User.create(email: "user2@gmail.com", password: "user2-pass202", role_id: 0)
+User.create(email: "user3@gmail.com", password: "user3-pass313", role_id: 0)
+User.create(email: "user4@gmail.com", password: "user4-pass424", role_id: 0)
+User.create(email: "user5@gmail.com", password: "user5-pass535", role_id: 0)
+User.create(email: "user6@gmail.com", password: "user6-pass646", role_id: 0)
+User.create(email: "user7@gmail.com", password: "user7-pass757", role_id: 0)
+User.create(email: "user8@gmail.com", password: "user8-pass868", role_id: 0)
+User.create(email: "admin@gmail.com", password: "admin-password816", role_id: 1)
 
 # 10.times do
 #     purchase_date = Date.parse('2019-08-10')
