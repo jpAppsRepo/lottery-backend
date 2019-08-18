da242f8e11dc895d33d9baf93314f060ffd159d6
jpAppsRepo <misheel@Misheels-MacBook-Pro.local>
2019-08-19 00:20:47 +0900

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -78,6 +78,10 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.digest = true
+
+  config.assets.css_compressor = :yui
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
