765dd41bf8953aeee8c26a304a93f8cbb0d29f90
jpAppsRepo <misheel@Misheels-MacBook-Pro.local>
2019-08-19 00:55:57 +0900

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,3 @@ User.create(email: "user6@gmail.com", password: "user6-pass646", role_id: 0)
 User.create(email: "user7@gmail.com", password: "user7-pass757", role_id: 0)
 User.create(email: "user8@gmail.com", password: "user8-pass868", role_id: 0)
 User.create(email: "admin@gmail.com", password: "admin-password816", role_id: 1)
-
-# 10.times do
-#     purchase_date = Date.parse('2019-08-10')
-#     location = rand(1..20)
-#     phone_number = Faker::PhoneNumber.cell_phone
-#     surname = Faker::Coffee.blend_name
-#     name = Faker::Coffee.blend_name
-#     product_name = Faker::Device.model_name
-#     price_info = rand(0..1000)
-#     phone_imei = Faker::Device.serial
-#     Item.create(purchase_date: purchase_date , location: location, phone_number: phone_number, surname: surname,
-#         name: name, product_name: product_name, price_info: price_info, phone_imei: phone_imei )
-# end
