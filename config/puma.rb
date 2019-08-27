bd06f3400f1bbe2ce6bda498fb9b5c283cd3b082
jpAppsRepo <misheel@Misheels-MacBook-Pro.local>
2019-08-27 19:26:37 +0900

    config for development

--- config/puma.rb
+++ config/puma.rb
@@ -4,11 +4,11 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 2 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
 threads threads_count, threads_count
 
 # Set up socket location
-bind "unix:///home/tushig/apps/lottery-backend/shared/tmp/sockets/puma.sock"
+# bind "unix:///home/tushig/apps/lottery-backend/shared/tmp/sockets/puma.sock"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -16,7 +16,7 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
