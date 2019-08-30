cf236b285f2729f78d73ad52bb3c5925a499fe30
jpAppsRepo <misheel@Misheels-MacBook-Pro.local>
2019-08-30 10:03:37 +0900

    forgery fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
     protect_from_forgery unless: -> { request.format.json? }
+    before_action :authenticate_user!
 
     private
     def after_sign_out_path_for(resource_or_scope)
