92366183f163a3d40bb800b933312ce8aa290c3b
jpAppsRepo <misheel@Misheels-MacBook-Pro.local>
2019-08-30 10:05:21 +0900

    forgery fix1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery unless: -> { request.format.json? }
+    protect_from_forgery with: :exception, prepend: true
     before_action :authenticate_user!
 
     private
