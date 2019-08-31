0608321e4ca822754f0294451b961fc7f10074ba
jpAppsRepo <misheel@Misheels-MacBook-Pro.local>
2019-08-31 21:27:39 +0900

    harmony mode fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.enabled = true
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Custom config
   # config.assets.css_compressor = :yui
   # config.assets.css_compressor = :sass
